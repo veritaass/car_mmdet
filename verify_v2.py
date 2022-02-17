5ce74682b446a43069094a172a74409ee3ce6835
veritaass <shapekim@gmail.com>
2022-02-17 17:33:04 +0900

    epoch3000

--- verify_v2.py
+++ verify_v2.py
@@ -14,7 +14,7 @@ def parse_args():
     parser.add_argument('--config', help='Config file'
         , default='configs/htkim_car/htkim_yolox_s_8x8_300e_coco_2nd.py')
     parser.add_argument('--checkpoint', help='Checkpoint file'
-        , default='work_dirs/htkim_yolox_s_8x8_300e_coco_2nd/epoch_1700.pth')
+        , default='work_dirs/htkim_yolox_s_8x8_300e_coco_2nd/epoch_3000.pth')
     parser.add_argument('--score-thr', type=float
         , default=0.2, help='bbox score threshold')
     args = parser.parse_args()
@@ -32,7 +32,7 @@ def main(args):
     checkpoint_file = args.checkpoint
     #score_thr=0.3
     
-    device = 'cuda:0'
+    device = 'cuda:1'
     # init a detector
     model = init_detector(config_file, checkpoint_file, device=device)
     # inference the demo image
@@ -41,8 +41,8 @@ def main(args):
     rst_inf = inference_detector(model, img)  # 'demo/demo.jpg')
     #print(rst_inf)
     # f_name = "res_" + "-".join(model.CLASSES) + "_score" + str(args.score_thr) + "_" + pathlib.Path(img).name
-    f_name = "res_" + pathlib.Path(img).name.split(".")[0] + "_score" + str(args.score_thr) + "_" + pathlib.Path(img).name
-    show_result_pyplot2(model, img, rst_inf, score_thr=args.score_thr, f_name)
+    f_name = "res_" + pathlib.Path(checkpoint_file).name.split(".")[0] + "_score" + str(args.score_thr) + "_" + pathlib.Path(img).name
+    show_result_pyplot2(model, img, rst_inf, score_thr=args.score_thr, f_name=f_name)
 
 def show_result_pyplot2(model,
                        img,
